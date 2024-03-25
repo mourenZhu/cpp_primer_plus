fa43d6a400313cadb8e904c846ccbd8068aaf16f
zhumouren <zhumouren0623@gmail.com>
2024-03-25 15:08:01 +0800

    完成第11章

---
+++ part11/stonewt.h
@@ -0,0 +1,19 @@
+#ifndef STONEWT_H_
+#define STONEWT_H_
+
+class Stonewt {
+private:
+    static const int LBS_per_stn = 14;
+    int stone;
+    double pds_left;
+    double pounds;
+public:
+    Stonewt(double lbs);
+    Stonewt(int stn, double lbs);
+    Stonewt();
+    ~Stonewt();
+    void show_lbs() const;
+    void show_stn() const;
+};
+
+#endif
